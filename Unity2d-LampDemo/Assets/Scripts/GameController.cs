23b06b16b1216cc722daede5cf81cfcadb42bc80
gdmec07150805 <1395243186@qq.com>
2017-04-21 13:22:08 +0800

    Demo

---
+++ Unity2d-LampDemo/Assets/Scripts/GameController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour {
+    public Item getItem;
+    private Item targetItem;
+    public Text gameInfo;
+    //这是一个线性表，用于存储（4*4）的灯泡对象
+    private List<Item> itemList;
+    private int tryCount = 0;
+    // Use this for initialization
+    void Start () {
+        itemList = new List<Item>();
+        for (int row=0;row<4;row++)
+        {
+            for(int col = 0; col < 4; col++)
+            {
+                targetItem = Instantiate(getItem) as Item;
+                targetItem.setController(this);
+                targetItem.UpdatePos(col-2,row-2);
+                itemList.Add(targetItem);
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    public void UpdateOtherLight(int rowIndex,int colIndex)
+    {
+        int total = 0;
+        
+        tryCount++;
+        Item temp = getLightItem(rowIndex + 1, colIndex);
+        if (temp!=null)
+        {
+            
+            temp.UpdateState();
+        }
+        temp = getLightItem(rowIndex - 1, colIndex);
+        if (temp != null)
+        {
+            
+            temp.UpdateState();
+        }
+        temp = getLightItem(rowIndex , colIndex +1);
+        if (temp != null)
+        {
+            
+            temp.UpdateState();
+        }
+        temp = getLightItem(rowIndex , colIndex -1);
+        if (temp != null)
+        {
+            
+            temp.UpdateState();
+        }
+
+        foreach(Item item in itemList)
+        {
+            if (item.getSate())
+            {
+                total += 1;
+            }
+        }
+        if (total == itemList.Count)
+        {
+            gameInfo.text = "经过了" + tryCount + "次的尝试，你终于赢了！";
+        }else
+        {
+            gameInfo.text = "你尝试了"+tryCount+"次，点亮了"+total+"盏灯";
+        }
+    }
+    public Item getLightItem(int rowIndex,int colIndex)
+    {
+        foreach (Item item in itemList) {
+            if (item.isMatch(rowIndex,colIndex))
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+}
---
+++ Unity2d-LampDemo/Assets/Scripts/Item.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+    public Sprite[] sprites;
+    private SpriteRenderer SpriteRd;
+    private bool isNo;
+    private int row;
+    private int col;
+
+    public GameController gController;
+    public void setController(GameController con)
+    {
+        this.gController = con;
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        //获取切换帧的渲染器
+        SpriteRd = GetComponent<SpriteRenderer>();
+        isNo = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseDown()
+    {
+        UpdateState();
+        gController.UpdateOtherLight(row,col);
+        Debug.Log(row+","+col);
+    }
+    public void UpdatePos(int row,int col)
+    {
+        this.row = row;
+        this.col = col;
+        this.transform.position = new Vector2(col,row);
+    }
+    public void UpdateState()
+    {
+        isNo = !isNo;
+        if (isNo)
+        {
+            SpriteRd.sprite = sprites[1];
+        }
+        else
+        {
+            SpriteRd.sprite = sprites[0];
+        }
+    }
+    public bool isMatch(int rowIndex,int colIndex)
+    {
+        if (this.row==rowIndex&&this.col==colIndex) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public bool getSate()
+    {
+        if (SpriteRd.sprite == sprites[1])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
